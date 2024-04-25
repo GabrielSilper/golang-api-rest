420e10a7f0f41913971e5d17a96a787a362da00d
GabrielSilper <gabrielsilper.main@gmail.com>
2024-04-24 23:27:18 -0400

    feat: Adicionei o Gorilla Mux e a rota GET para /artists/{id}

--- controllers/artistController/artist-controller.go
+++ controllers/artistController/artist-controller.go
@@ -3,10 +3,28 @@ package artistController
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/gabrielsilper/golang-api-rest/models"
+	"github.com/gorilla/mux"
 )
 
 func GetAll(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(models.ArtistSlice)
+}
+
+func GetById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var artist models.Artist
+
+	for _, v := range models.ArtistSlice {
+		if strconv.Itoa(v.ID) == id {
+			artist = v
+			break
+		}
+	}
+
+	json.NewEncoder(w).Encode(artist)
 }
--- main.go
+++ main.go
@@ -1,23 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-
 	"github.com/gabrielsilper/golang-api-rest/routes"
 )
 
 const (
-	port = 8000
+	port = 8080
 )
 
-func startServer() {
-	routes.HandleRequests()
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
-}
-
 func main() {
-	fmt.Println("Server listening port", port)
-	startServer()
+	routes.StartServer(port)
 }

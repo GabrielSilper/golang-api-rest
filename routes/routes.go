4f2a190466878ed77a31fe2ed122c3943b435ea9
GabrielSilper <gabrielsilper.main@gmail.com>
2024-04-26 22:00:52 -0400

    feat: Aprendi como adiciona middleware e a política de cors

--- routes/routes.go
+++ routes/routes.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 
 	"github.com/gabrielsilper/golang-api-rest/controllers/artistController"
+	"github.com/gabrielsilper/golang-api-rest/middlewares"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -14,10 +16,12 @@ var router = mux.NewRouter()
 func StartServer(port int) {
 	HandleArtistsRequests()
 	fmt.Println("Server listening port", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), router))
+	stringPort := fmt.Sprintf(":%d", port)
+	log.Fatal(http.ListenAndServe(stringPort, handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(router)))
 }
 
 func HandleArtistsRequests() {
+	router.Use(middlewares.ContentTypeMiddleware)
 	router.HandleFunc("/api/artists", artistController.GetAll).Methods(http.MethodGet)
 	router.HandleFunc("/api/artists/{id}", artistController.GetById).Methods(http.MethodGet)
 	router.HandleFunc("/api/artists", artistController.Create).Methods(http.MethodPost)
